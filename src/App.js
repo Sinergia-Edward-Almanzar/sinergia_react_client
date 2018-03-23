ecbdbd70f1c0ec1af405d997e3c2836a1b504b59
Sinergia-Edward-Almanzar <37590212+Sinergia-Edward-Almanzar@users.noreply.github.com>
2018-03-23 14:17:09 -0400

    react burger menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import sinergialogo from './sinergia_logo.png';
 import './App.css';
+import './burgermenu.css';
+import { bubble as Menu } from 'react-burger-menu'
+import Burgermenu from './components/burgermenu';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+    <div id="outer-container">
+    <Menu
+      pageWrapId={ "page-wrap" }
+      outerContainerId={ "outer-container" }
+      >
+      <img src={sinergialogo} className="App-logo" alt="logo" />
+      <a id="home" className="menu-item" href="/">HOME</a>
+      <a id="about" className="menu-item" href="/about">WHO WE ARE</a>
+      <a id="about" className="menu-item" href="/about">NEWS AND EVENTS</a>
+      <a id="about" className="menu-item" href="/about">SUCCES STORIES</a>
+      <a id="about" className="menu-item" href="/about">GET INVOLVED</a>
+      <a id="about" className="menu-item" href="/about">SUPPORT US</a>
+      <a id="about" className="menu-item" href="/about">DONATE</a>
+      <a id="contact" className="menu-item" href="/contact">CONTACT</a>
+      <a id="about" className="menu-item" href="/about">CARRERS</a>
+      <a onClick={ this.showSettings } className="menu-item--small" href="">SETTINGS</a>
+    </Menu>
+
+
+  <main id="page-wrap">
+      <div>
+      <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+      <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+        <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+        <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+          <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+          <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+            <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+            <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+              <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+              <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+                <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+                <h2> duibfiubefgyueibgiweugbwiuebguiwebguiwebgiuwebgiuwe</h2>
+                  <h1>dbqidbquwbduqwibfiuqbfuiqwbfqiuwfbiuqwbfiuqwbf</h1>
+
       </div>
+    </main>
+  </div>
     );
   }
 }
